91b2ef746b6a37a99b764a08c13e1715696b142d
Ryu Suzuki <r216yu@gmail.com>
2023-07-01 19:16:33 +0900

    Footerを最下部に固定

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ import { Box, Container, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ py: 4, borderTop: "1px solid" }}>
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
       <Container>
         <Box sx={{ textAlign: "center" }}>
           <Typography variant="caption">&copy;2023 S.RYU</Typography>
